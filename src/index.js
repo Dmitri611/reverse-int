8709b916d0037a3c5bf0278b0f3b88e9dabe7282
Dmitri611 <dimakkk1181@mail.ru>
2020-10-20 23:04:44 +0300

    feat: complited task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {n*=-1};
+  let   arr = String(n).split('').reverse();
+  if (arr[arr.length] === 0){
+      delete arr[arr.length];
+  }
+  return +arr.join('')
 }
